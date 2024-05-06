06ba8571d4628f897e12dc138250a79d2b637df1
goatney420 <84412113+goatney420@users.noreply.github.com>
2024-05-06 13:45:57 -0700

    Update weather.tsx

--- my-app/src/weather.tsx
+++ my-app/src/weather.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import { coldActivity, moderateActivity, warmActivity, ActivityItem } from './data/activity-data';
 
 const Weather: React.FC = () => {
   const [weatherData, setWeatherData] = useState<any>(null);
@@ -12,7 +13,6 @@ const Weather: React.FC = () => {
         `https://api.openweathermap.org/data/2.5/weather?q=New York City&units=metric&appid=${apiKey}`
       );
       setWeatherData(response.data);
-      console.log(response.data); 
     } catch (error) {
       console.error(error);
     }
@@ -20,16 +20,45 @@ const Weather: React.FC = () => {
 
   const convertToFarhenheit = (celsius: number) => {
     const fahrenheit = (celsius * 9 / 5) + 32;
-    return fahrenheit.toFixed(0); // Round to one decimal place
+    return parseFloat(fahrenheit.toFixed(0));
   };
 
   return (
     <div className="weather-container">
-      <button className="weather-button" onClick={fetchData}>See NYC Weather</button>
-      {weatherData !== null && (
-        <>
-          <h2>{convertToFarhenheit(weatherData.main.temp)}°F</h2>
-        </>
+      <div className="weather-header">
+        <button className="weather-button" onClick={fetchData}>See NYC Weather</button>
+        {weatherData && (
+          <>
+            <h2 className='temp'>{convertToFarhenheit(weatherData.main.temp)}°F</h2>
+            <h2>{weatherData.weather[0].description}</h2>
+          </>
+        )}
+      </div>
+      {weatherData && (
+        <ul>
+          {convertToFarhenheit(weatherData.main.temp) < 50 ? (
+            coldActivity.map((item: ActivityItem, index: number) => (
+              <li key={index}>
+                <span>{item.name}</span>
+                {item.price && <span className="price">free</span>}
+              </li>
+            ))
+          ) : convertToFarhenheit(weatherData.main.temp) >= 50 && convertToFarhenheit(weatherData.main.temp) < 70 ? (
+            moderateActivity.map((item: ActivityItem, index: number) => (
+              <li key={index}>
+                <span>{item.name}</span>
+                {item.price && <span className="price">free</span>}
+              </li>
+            ))
+          ) : (
+            warmActivity.map((item: ActivityItem, index: number) => (
+              <li key={index}>
+                <span>{item.name}</span>
+                {item.price && <span className="price">free</span>}
+              </li>
+            ))
+          )}
+        </ul>
       )}
     </div>
   );
