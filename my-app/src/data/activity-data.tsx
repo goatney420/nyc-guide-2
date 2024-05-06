8c82cea770fd53bd91c133739ee1f8e9f26f51c5
goatney420 <84412113+goatney420@users.noreply.github.com>
2024-05-06 13:45:44 -0700

    Update activity-data.tsx

--- my-app/src/data/activity-data.tsx
+++ my-app/src/data/activity-data.tsx
@@ -3,16 +3,30 @@ export interface ActivityItem {
     price: boolean; 
   }
   
-  export const activity: ActivityItem[] = [
-    { name: "Run along WSH", price: true }, 
-    { name: "Explore Central Park", price: true }, 
-    { name: "See a brodway show", price: false }, 
-    { name: "Walk around Dumbo", price: true }, 
-    { name: "CitiBike along WSH", price: false }, 
-    { name: "Shop till you drop", price: false }, 
-    { name: "Sit in Washington Square Park and observe NYU dorks", price: true }, 
-    { name: "Enjoy the subway creatures", price: true }, 
-    { name: "Workout at CFUS with me", price: false }, 
-    { name: "Walk around the Little Island while on shrooms", price: true }, 
-    { name: "Thrift in BK", price: false }, 
-  ];
+// below 49 degrees
+export const coldActivity: ActivityItem[] = [
+  { name: "See a brodway show", price: false }, 
+  { name: "Enjoy the subway creatures", price: true }, 
+  { name: "Workout at CFUS with me", price: false }, 
+  { name: "Shop till you drop", price: false },  
+];
+
+// 50 - 69 degrees
+export const moderateActivity: ActivityItem[] = [
+  { name: "See a brodway show", price: false },
+  { name: "Walk around Dumbo at night", price: true }, 
+  { name: "Shop till you drop", price: false },  
+  { name: "Workout at CFUS with me", price: false },
+  { name: "Walk around the Little Island while on shrooms", price: true }, 
+  { name: "Thrift in BK", price: false },  
+];
+
+// above 70 degrees
+export const warmActivity: ActivityItem[] = [
+  { name: "Run along WSH", price: true }, 
+  { name: "Explore Central Park", price: true }, 
+  { name: "CitiBike along WSH", price: false }, 
+  { name: "Sit in Washington Square Park and observe NYU dorks", price: true },
+];
+
+  
